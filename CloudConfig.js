a57a5c59dc93100c5f3f1ec71b15964f2c5d87da
Kavish284 <kavishkothari10@gmail.com>
2024-03-22 16:17:42 -0400

    formatted env

--- CloudConfig.js
+++ CloudConfig.js
@@ -5,16 +5,13 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET,
-    api_environment_variable:process.env.CLOUDINARY_URL
+    api_secret: process.env.CLOUD_API_SECRET
 })
 
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: async (req, file) => {
-      // async code using `req` and `file`
-      // ...
       return {
         folder: 'wanderlust_DEV',
         allowedFormats: ["png","jpg","jpeg"],
